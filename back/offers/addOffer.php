02f3adcc4f2ce69750cee036c038ac3b0e9937ef
DmitriyLobov <dimkalobov95@gmail.com>
2015-06-10 09:03:14 +0300

    fix adding offer

--- back/offers/addOffer.php
+++ back/offers/addOffer.php
@@ -7,12 +7,12 @@
 	$postdata = file_get_contents("php://input");
 	$POST = json_decode($postdata);
 
-	$sql = "INSERT INTO `offer`(`name`,`id_user`, `brand`, `img`, `price`, `url`, `catalog`, `status`, `additional`) VALUES ('$POST->name','$POST->user','$POST->brand','$POST->img','$POST->price','$POST->url','$POST-catalog','$POST->status','$POST->additional')";
+	$sql = "INSERT INTO `offer`(`name`,`id_user`, `brand`, `img`, `price`, `url`, `catalog`, `status`, `additional`) VALUES ('$POST->name','$POST->user','$POST->brand','$POST->img','$POST->price','$POST->url','$POST-catalog','0','$POST->additional')";
 
 	if(mysqli_query($mysql,$sql)){
 		$id = mysqli_insert_id($mysql);
 		for($i = 0;$i< count($POST->values);$i++){
-			$sql = "INSERT INTO `offertorule`(`id_offer`, `id_rule`, `value`) VALUES ('$id','".$POST->charact[$i]."','".$POST->val[$i]."')";
+			$sql = "INSERT INTO `offertorule`(`id_offer`, `id_rule`, `value`) VALUES ('$id','".$POST->values[$i]->charact."','".$POST->values[$i]->val."')";
 			mysqli_query($mysql,$sql);
 		}
 
